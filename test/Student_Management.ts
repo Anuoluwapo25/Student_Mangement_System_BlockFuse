148e9812851d7dee8b85c98d72ce41fe1e2ac1cf
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2025-02-07 03:08:08 +0100

    implemeneted testing for getstudents

--- test/Student_Management.ts
+++ test/Student_Management.ts
@@ -128,6 +128,33 @@ import {
         );
   
     });
+
+    it("Should give custom error when getting a student that does not exist", async function () {
+      const { 
+        studentManagementContract,
+        owner,
+        account1,
+        account2,
+        account3
+       } = await loadFixture(deployStudentManagement);
+
+        const name = "john doe";
+        const age = 12;
+        const studentClass = "js1";
+        const gender = 0;
+
+       await studentManagementContract.registerStudent(
+        name,
+        age,
+        studentClass,
+        gender);
+      
+      await expect(studentManagementContract.getStudent(5)).to.be.revertedWithCustomError(
+        studentManagementContract,
+        "CouldNotGetStudent()"
+      );
+
+  });
   
     it("Should fails when registering student with non admin account", async function () {
       const { 
@@ -151,86 +178,127 @@ import {
        
     
   });
-  
-  it("Should fails when updating student with non admin account", async function () {
+
+
+    it("Should return all students in Student", async function () {
     const { 
       studentManagementContract,
       owner,
       account1,
       account2,
       account3
-     } = await loadFixture(deployStudentManagement);
+    } = await loadFixture(deployStudentManagement);
 
-      const name = "john doe";
-      const age = 12;
-      const studentClass = "js1";
-      const gender = 0;
+    const name = "john doe";
+    const age = 12;
+    const studentClass = "js1";
+    const gender = 0;
 
-  await expect(studentManagementContract.connect(account1).registerStudent(
-    name,
-    age,
-    studentClass,
-    gender)).to.be.revertedWith("You are not the admin");
-     
-  
-});
+    await studentManagementContract.registerStudent(
+        name,
+        age,
+        studentClass,
+        gender);
 
-it("Should fails when deleting student with non admin account", async function () {
-    const { 
-      studentManagementContract,
-      owner,
-      account1,
-      account2,
-      account3
-     } = await loadFixture(deployStudentManagement);
+    const name2 = "jane smith";
+    const age2 = 13;
+    const studentClass2 = "js2";
+    const gender2 = 1;
 
-      const name = "john doe";
-      const age = 12;
-      const studentClass = "js1";
-      const gender = 0;
+    await studentManagementContract.registerStudent(
+        name2,
+        age2,
+        studentClass2,
+        gender2);
 
-  await expect(studentManagementContract.connect(account1).registerStudent(
-    name,
-    age,
-    studentClass,
-    gender)).to.be.revertedWith("You are not the admin");
-     
-  
-});
+    await studentManagementContract.getStudents();
 
-it("Should updatd student with valid id ", async function () {
-    const { 
-      studentManagementContract,
-      owner,
-      account1,
-      account2,
-      account3
-     } = await loadFixture(deployStudentManagement);
+    const student = await studentManagementContract.getStudents();
 
-     const name = "john doe";
-          const age = 12;
-          const studentClass = "js1";
-          const gender = 0;
-  
-         await studentManagementContract.registerStudent(
-          name,
-          age,
-          studentClass,
-          gender);
+    expect(await student[0].name).to.eq("john doe")
+    expect(await student[1].age).to.eq(13)
+    expect(await student[1].name).to.eq("jane smith")
 
-      const studentId = 1
-      const Class = "js2";
+    });
 
-      await studentManagementContract.updateStudentClass(studentId, Class);
+    it("Should return empty if no registered students in Student", async function() {
+      const {
+        studentManagementContract,
+        owner,
+        account1,
+        account2,
+        account3,
+      } = await loadFixture(deployStudentManagement);
+
+      const students = await studentManagementContract.getStudents();
+
+      expect(await students.length).to.eq(0)
+    });
+
+    it("Should delete students in Student", async function() {
+      const {
+        studentManagementContract,
+        owner,
+        account1,
+        account2,
+        account3,
+      } = await loadFixture(deployStudentManagement);
+
+        const name3 = "Emeke Chidera";
+        const age3 = 16;
+        const studentClass3 = "ss2";
+        const gender3 = 0;
+
+      await studentManagementContract.registerStudent(
+        name3,
+        age3,
+        studentClass3,
+        gender3,
+      );
+
+      const studentid = 1
+
+      await studentManagementContract.deleteStudent(studentid);
+
+      const student = await studentManagementContract.getStudents();
+
+      expect(student[0].name).to.equal("Emeke Chidera");
+    })
+
+
+    it("Should updatd student with valid id ", async function () {
+        const { 
+          studentManagementContract,
+          owner,
+          account1,
+          account2,
+          account3
+        } = await loadFixture(deployStudentManagement);
+
+        const name = "john doe";
+        const age = 12;
+        const studentClass = "js1";
+        const gender = 0;
       
-      const student = await studentManagementContract.getStudent(1);
+        await studentManagementContract.registerStudent(
+            name,
+            age,
+            studentClass,
+            gender);
 
-       expect(await student.class).to.eq("js2")
+        const studentId = 1
+        const Class = "js2";
 
-    
-     
-  
-});
+        await studentManagementContract.updateStudentClass(studentId, Class);
+        
+        const student = await studentManagementContract.getStudent(1);
+
+        expect(await student.class).to.eq("js2")
+
+        
+        
+      
+    });
 
 
 
